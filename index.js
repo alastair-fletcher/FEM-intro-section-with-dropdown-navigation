610b0fb1df44a5ce3f005158ece2c89b274052b3
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-12-16 16:37:06 +0100

    feat: add  mobile layout

---
+++ index.js
@@ -0,0 +1,35 @@
+// ============= Side menu
+const hamburger = document.querySelector('.hamburger');
+const closeButton = document.querySelector('.close-button');
+const sideNav = document.querySelector('.side-nav');
+const modal = document.querySelector('.modal');
+const sideNavDropDowns = document.querySelectorAll('.side-nav-drop-down');
+const toggleDropDownMenus = document.querySelectorAll('.toggle-drop-down');
+
+function toggleMobileMenu() {
+  sideNav.classList.toggle('hide');
+  modal.classList.toggle('hide');
+  sideNavDropDowns.forEach((el) => {
+    !el.classList.contains('hideDropDown') && el.classList.add('hideDropDown');
+  });
+  toggleDropDownMenus.forEach((el) => {
+    el.src = '/images/icon-arrow-down.svg';
+  });
+}
+
+hamburger.addEventListener('click', toggleMobileMenu);
+closeButton.addEventListener('click', toggleMobileMenu);
+
+// ============= Dropdowns
+function toggleDropDown(e) {
+  let dropDown = document.createTreeWalker(e.target).currentNode.parentNode
+    .nextElementSibling;
+  dropDown.classList.toggle('hideDropDown');
+  dropDown.classList.contains('hideDropDown')
+    ? (e.target.src = '/images/icon-arrow-down.svg')
+    : (e.target.src = '/images/icon-arrow-up.svg');
+}
+
+toggleDropDownMenus.forEach((el) =>
+  el.addEventListener('click', toggleDropDown)
+);
